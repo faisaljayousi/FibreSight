c26da31d2a0303d3f6e50410aedd6ae50fb127e2
Faisal Jayousi <fjayousi90@gmail.com>
2024-08-13 17:21:50 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@
 
 # Author: Faisal Jayousi
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        packages=find_packages(),
+        install_requires=[
+            "numpy>=1.25.0",
+            "skan==0.11.0",
+            "skl-graph==2023.15",
+        ],
+    )
