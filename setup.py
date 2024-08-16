f977e2c411a53695e30ff144083ced0fbbd67b60
Faisal Jayousi <fjayousi90@gmail.com>
2024-08-16 14:32:47 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,7 @@
 
 # Author: Faisal Jayousi
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        packages=find_packages(),
-        install_requires=[
-            "numpy>=1.25.0",
-            "skan==0.11.0",
-            "skl-graph==2023.15",
-        ],
-    )
+    setup()
